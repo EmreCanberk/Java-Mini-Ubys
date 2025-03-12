8e67ee7da1176613291d3d8ab2a7040e10519bbf
Emre Canberk Tok <149680770+EmreCanberk@users.noreply.github.com>
2025-03-12 15:38:15 +0300

    Update Lesson.java

--- Lesson.java
+++ Lesson.java
@@ -1,49 +1,70 @@
 package Run;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Lesson {
-	private String name ;
-	private int id ;
-	private byte credit ;
-	private ArrayList<Student> students = new ArrayList<>();
-	public Lesson(String name, int id, byte credit) {
-		this.name = name;
-		this.id = id;
-		this.credit = credit;
-	}
-	public void registerStudent(Student s) {
-		students.add(s);
-		
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public byte getCredit() {
-		return credit;
-	}
-	public void setCredit(byte credit) {
-		this.credit = credit;
-	}
-	public void printLesson() {
-		System.out.println(this.id+"-"+this.name);
-	}
-	
-	public void printStudentList() {
-		System.out.println(name+" dersini alan öðreci listesi");
-		System.out.println("--------------------------------------");
-		for (Student s : this.students) {
-			s.printStudent();
-		}
-	}
-	
+    private static int idCounter = 100; 
+    private String name;
+    private int id;
+    private List<Student> students;
+    private List<Grades> grades; 
+
+    public Lesson(String name) {
+        this.name = name;
+        this.id = getNextId(); 
+        this.students = new ArrayList<>();
+        this.grades = new ArrayList<>(); 
+    }
+
+    private static int getNextId() {
+        return idCounter++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void printLessonInfo() {
+        System.out.println("Ders: " + name + " - Ders ID: " + id);
+    }
+
+    public void printStudentsTakingLesson() {
+        System.out.println("Dersi alan öğrenciler:");
+        for (Student student : students) {
+            System.out.println("  - " + student.getFirstName() + " " + student.getLastName());
+        }
+    }
+
+    public void addGrade(Grades grade) {
+        grades.add(grade);
+    }
+
+    public boolean hasGrade(Student student) {
+        for (Grades grade : grades) {
+            if (grade.getStudent().equals(student)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void printGrades() {
+        System.out.println("Ders: " + name + " - Notlar:");
+        for (Grades grade : grades) {
+            System.out.println("  - Öğrenci: " + grade.getStudent().getFirstName() + " " + grade.getStudent().getLastName() + ", Vize: " + grade.getvizeNot() + ", Final: " + grade.getFinalNot());
+        }
+    }
 }
